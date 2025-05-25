9c570bebb4bdbf526e405c8dde5c43a92650f44e
Anastasia Korysheva <nastiakor06@gmail.com>
2025-05-25 13:25:19 +0300

    predstavlenia

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                    .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     
     class Status(models.TextChoices):
@@ -26,6 +31,9 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
         )
+
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ['-publish']
