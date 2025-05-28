89e3289afc6d968ea52d511696091d0003b3cb16
Anastasia Korysheva <nastiakor06@gmail.com>
2025-05-28 10:51:57 +0300

    number 4

--- myexam/examsite/admin.py
+++ myexam/examsite/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import akexam
+from django.contrib.auth.models import User
 
 @admin.register(akexam)
 class akexamAdmin(admin.ModelAdmin):
-    list_display = ("exam_name", "exam_date", "is_public")
-    list_filter = ("is_public", "exam_date")
-    search_fields = ("exam_name",)
+    search_fields = ['exam_name', 'assigned_users__email']
+    
+    date_hierarchy = 'exam_date'
+    
+    filter_horizontal = ['assigned_users']
+    
+    list_filter = ['is_public']
+    
+    list_display = ['exam_name', 'exam_date', 'is_public']
+    list_editable = ['is_public']
